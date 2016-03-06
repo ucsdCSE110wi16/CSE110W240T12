3cfb22b6bf41de59b681bd9f8dead4382769c6c7
Miguel Angel Vargas <miguelvargas.121@gmail.com>
2016-03-05 17:08:42 -0800

    UPDATE: Active Group displays manager's name, not email

--- GiftExchangeApplication/app/src/main/java/cse110/giftexchangeapplication/ui/activeGroups/ActiveGroupAdapter.java
+++ GiftExchangeApplication/app/src/main/java/cse110/giftexchangeapplication/ui/activeGroups/ActiveGroupAdapter.java
@@ -4,12 +4,16 @@ import android.app.Activity;
 import android.view.View;
 import android.widget.TextView;
 
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 import com.firebase.client.Query;
+import com.firebase.client.ValueEventListener;
 import com.firebase.ui.FirebaseListAdapter;
 
 import cse110.giftexchangeapplication.R;
 import cse110.giftexchangeapplication.model.ActiveGroup;
+import cse110.giftexchangeapplication.model.User;
 import cse110.giftexchangeapplication.utils.Constants;
 
 public class ActiveGroupAdapter extends FirebaseListAdapter<ActiveGroup> {
@@ -34,13 +38,24 @@ public class ActiveGroupAdapter extends FirebaseListAdapter<ActiveGroup> {
         /**
          * Grab the needed TextViews and strings
          */
-        TextView textViewGroupName = (TextView) view.findViewById(R.id.text_view_group_name);
-        TextView textViewManagedByUser = (TextView) view.findViewById(R.id.text_view_managed_by_user);
+        final TextView textViewGroupName = (TextView) view.findViewById(R.id.text_view_group_name);
+        final TextView textViewManagedByUser = (TextView) view.findViewById(R.id.text_view_managed_by_user);
 
         Firebase ref = new Firebase(Constants.FIREBASE_URL_USERS).child(group.getManager());
-        
+        ValueEventListener refListener = ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+
+                textViewManagedByUser.setText(user.getName());
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
         // Set the group name & owner
         textViewGroupName.setText(group.getGroupName());
-        textViewManagedByUser.setText(ref.);
     }
 }
